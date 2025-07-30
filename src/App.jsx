6349a2781ce8d23c1422668e90bd896558ae3bae
Niraj kumar <nirajnil2@gmail.com>
2025-07-30 10:46:03 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,30 @@ import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 const Dashboard = lazy(() => import('./components/Dashboard'));
 const Landing = lazy(() => import('./components/Landing'));
+import { Countcontext } from './context';
+import { useContext , useState } from 'react';
 
 function App() {
   
-  return (
+ /*  return (
     <div>
-     
        <BrowserRouter>
        <Appbar/>
       <Routes>
-        <Route path="/Landing" element={<Landing />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+       <Route path="/landing" element={
+  <Suspense fallback={"Loading..."}>
+    <Landing />
+  </Suspense>
+} />
+        <Route path="/dashboard" element={
+  <Suspense fallback={"Loading..."}>
+    <Dashboard/>
+  </Suspense>
+} />
       </Routes>
     </BrowserRouter>
     </div>
-
-  ); 
+  );
 }
 function Appbar(){
   const navigate = useNavigate();
@@ -32,6 +40,50 @@ function Appbar(){
           navigate("/Dashboard")
         }}>Dashboard</button>
       </div>
+    </div>
+  )} */
+
+    const [count , setcount] = useState(0);
+    
+    return (
+      <div>
+       <Countcontext.Provider value={count} >
+        <Count  count={count} setcount={setcount} />
+        </Countcontext.Provider>
+      </div>
+    )
+}
+function Count({setcount}){
+  return (
+    <div>
+      <Countrenderer/>
+      <Button setcount={setcount} />
+    </div>
+  )
+}
+function Countrenderer(){
+  const count = useContext(Countcontext);
+  return (
+    <div>
+      count : {count}
+    </div>
+  )
+}
+function Button({setcount}){
+  const count = useContext(Countcontext);
+  return (
+    <div>
+      <button onClick={ ()=>{
+        setcount(count+1) ;
+      }}   > Increase
+
+      </button>
+      <button onClick={ ()=>{
+        setcount(count-1) ;
+      }}   > Decrease 
+
+      </button>
+
     </div>
   )
 }
